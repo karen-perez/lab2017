fcdc3ba17a76a3bba81981899b9cae602f5bdeef
Karen <Karen@Karen-PC>
2017-08-19 00:30:25 -0300

    agregado funcionalidad editar un contacto (falta ajustar codigo)

--- Agenda/src/presentacion/vista/VentanaPersona.java
+++ Agenda/src/presentacion/vista/VentanaPersona.java
@@ -18,6 +18,7 @@ import java.awt.Color;
 import com.toedter.calendar.JDateChooser;
 
 import dto.LocalidadDTO;
+import dto.PersonaDTO;
 import dto.TipoContactoDTO;
 
 public class VentanaPersona extends JFrame 
@@ -35,13 +36,19 @@ public class VentanaPersona extends JFrame
 	private JTextField textMail;
 	private JTextField textPiso;
 	private JDateChooser dateChooser;
+	private PersonaDTO persona_a_editar;
 	private JComboBox<LocalidadDTO> listaLocalidades;
 	private JComboBox <TipoContactoDTO> listaTipoContacto;
+	private boolean editar=false;
+	private JButton btnActualizarC;
 
-	public VentanaPersona(Controlador controlador) 
+
+	public VentanaPersona(Controlador controlador, DefaultComboBoxModel<LocalidadDTO> localidades, 
+			DefaultComboBoxModel<TipoContactoDTO> tipoContacto, PersonaDTO personaAmodificar) 
 	{
 		super();
 		this.controlador = controlador;
+		this.persona_a_editar = personaAmodificar;
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 626, 463);
@@ -73,10 +80,25 @@ public class VentanaPersona extends JFrame
 		panel.add(txtApellido);
 		txtApellido.setColumns(10);
 		
-		btnAgregarPersona = new JButton("Agregar");
-		btnAgregarPersona.addActionListener(this.controlador);
-		btnAgregarPersona.setBounds(452, 326, 107, 46);
-		panel.add(btnAgregarPersona);
+		if (personaAmodificar != null) { 
+			/*btnAgregarPersona = new JButton("Actualizar");
+			btnAgregarPersona.addActionListener(this.controlador);
+			btnAgregarPersona.setBounds(453, 269, 89, 23);
+			panel.add(btnAgregarPersona);
+				*/
+		btnActualizarC = new JButton("Actualizar");
+		btnActualizarC.addActionListener(this.controlador);
+		btnActualizarC.setBounds(278, 243, 97, 25);
+		panel.add(btnActualizarC);
+		} 
+		else{
+			btnAgregarPersona = new JButton("Agregar");
+			btnAgregarPersona.addActionListener(this.controlador);
+			btnAgregarPersona.setBounds(452, 326, 107, 46);
+			panel.add(btnAgregarPersona);
+			
+			
+		}
 		
 		textTelefono = new JTextField();
 		textTelefono.setColumns(10);
@@ -162,7 +184,38 @@ public class VentanaPersona extends JFrame
 		panel.add(listaLocalidades);		
 		
 		this.setVisible(true);
+		if (personaAmodificar != null) {
+			editar=true;
+			CargarDatosPersona(personaAmodificar);
+		}
+		
+		this.setVisible(true);
 	}
+	private void CargarDatosPersona(PersonaDTO personaAEditar) {
+		getTxtNombre().setText(personaAEditar.getNombre());
+		getTxtApellido().setText(personaAEditar.getApellido());
+		getTextTelefono().setText(personaAEditar.getTelefono());
+		getTextMail().setText(personaAEditar.getMail());
+		getDateChooser().setDate(personaAEditar.getFechanac());
+		getTextCalle().setText(personaAEditar.getCalle());
+		getTextAltura().setText(personaAEditar.getAltura());
+		getTextPiso().setText(personaAEditar.getPiso());
+		getTextDepto().setText(personaAEditar.getDepto());
+		getListaLocalidades().setSelectedItem(personaAEditar.getLocalidad());
+		getListaTipoContacto().setSelectedItem(personaAEditar.getTipocontacto());
+		
+		
+		
+	}
+	public boolean isEditar() {
+		return editar;
+	}
+
+	public void setEditar(boolean editar) {
+		this.editar = editar;
+	}
+
+	
 	public JTextField getTxtNombre() {
 		return txtNombre;
 	}
@@ -262,6 +315,11 @@ public class VentanaPersona extends JFrame
 		this.listaTipoContacto = listaTipoContacto;
 	}
 	
+	public PersonaDTO getPersonaAEditar() {
+		return persona_a_editar;
+	}
+
+
 
 	
 	
