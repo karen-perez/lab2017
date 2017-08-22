2259bcbda860b96fe762988ed949c4ab283ce469
NiDiBiase <NiDiBiase@DELL-72-5H3-JV.swm.com.ar>
2017-08-22 12:22:44 +0000

    Modificacion equals - No puede haber 2 localidades con mismo nombre

--- Agenda/src/dto/LocalidadDTO.java
+++ Agenda/src/dto/LocalidadDTO.java
@@ -56,6 +56,8 @@ public boolean equals(Object obj) {
 	if (getClass() != obj.getClass())
 		return false;
 	LocalidadDTO other = (LocalidadDTO) obj;
+	if (localidad.toLowerCase().equals(other.localidad.toLowerCase()))
+		return true;	
 	if (idLocalidad != other.idLocalidad)
 		return false;	
 	return true;
